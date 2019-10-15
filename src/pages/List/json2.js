8de5970fe07ff0a0adaab10e3a7c6efb9f0911a0
zhaoguangxue <zhaoguangxue@hylink.net.cn>
2019-10-15 21:16:14 +0800

    业务情况

---
+++ src/pages/List/json2.js
@@ -0,0 +1,70 @@
+export const pieDataShow = [
+  { y: 1, x: "刑事" },
+  { y: 20, x: "行政" },
+  { y: 15, x: "其他" }
+];
+export const chartData = [
+  {
+    x: '10-02',
+    y1: 58,
+    y2: 10,
+  },
+  {
+    x: '10-03',
+    y1: 36,
+    y2: 5,
+  },
+  {
+    x: '10-04',
+    y1: 58,
+    y2: 72,
+  },
+  {
+    x: '10-05',
+    y1: 25,
+    y2: 48,
+  },
+  {
+    x: '10-06',
+    y1: 58,
+    y2: 26,
+  },
+  {
+    x: '10-07',
+    y1: 74,
+    y2: 10,
+  },
+  {
+    x: '10-08',
+    y1: 58,
+    y2: 64,
+  },
+];
+export const tableData = [
+  {
+    ajlx: "行政",
+    ajly: 1,
+    ajmc: "张三盗窃案",
+    ajbh: 'BHYN4125378',
+    entrycause: "盗窃",
+    name: "宋佳",
+    xh: 1,
+  }, {
+    ajlx: "刑事",
+    ajly: 1,
+    ajmc: "小区盗窃案",
+    ajbh: 'BHYN2034512',
+    entrycause: "抢劫",
+    name: "陈平",
+    xh: 2,
+  },
+  {
+    ajlx: "民事",
+    ajly: 1,
+    ajmc: "闹事打架",
+    ajbh: 'BHYN2034512',
+    name: "王强",
+    entrycause: "闹事",
+    xh: 3,
+  }
+]
