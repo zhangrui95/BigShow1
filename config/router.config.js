589c0966a64cae2095c386437651df751a3e4e83
thkyo <thkyo@163.com>
2019-10-16 15:28:43 +0800

    隐藏菜单

--- config/router.config.js
+++ config/router.config.js
@@ -246,92 +246,92 @@ export default [
           },
         ],
       },
-      {
-        name: 'exception',
-        icon: 'warning',
-        path: '/exception',
-        routes: [
-          // exception
-          {
-            path: '/exception/403',
-            name: 'not-permission',
-            component: './Exception/403',
-          },
-          {
-            path: '/exception/404',
-            name: 'not-find',
-            component: './Exception/404',
-          },
-          {
-            path: '/exception/500',
-            name: 'server-error',
-            component: './Exception/500',
-          },
-          {
-            path: '/exception/trigger',
-            name: 'trigger',
-            hideInMenu: true,
-            component: './Exception/TriggerException',
-          },
-        ],
-      },
-      {
-        name: 'account',
-        icon: 'user',
-        path: '/account',
-        routes: [
-          {
-            path: '/account/center',
-            name: 'center',
-            component: './Account/Center/Center',
-            routes: [
-              {
-                path: '/account/center',
-                redirect: '/account/center/articles',
-              },
-              {
-                path: '/account/center/articles',
-                component: './Account/Center/Articles',
-              },
-              {
-                path: '/account/center/applications',
-                component: './Account/Center/Applications',
-              },
-              {
-                path: '/account/center/projects',
-                component: './Account/Center/Projects',
-              },
-            ],
-          },
-          {
-            path: '/account/settings',
-            name: 'settings',
-            component: './Account/Settings/Info',
-            routes: [
-              {
-                path: '/account/settings',
-                redirect: '/account/settings/base',
-              },
-              {
-                path: '/account/settings/base',
-                component: './Account/Settings/BaseView',
-              },
-              {
-                path: '/account/settings/security',
-                component: './Account/Settings/SecurityView',
-              },
-              {
-                path: '/account/settings/binding',
-                component: './Account/Settings/BindingView',
-              },
-              {
-                path: '/account/settings/notification',
-                component: './Account/Settings/NotificationView',
-              },
-            ],
-          },
-        ],
-      },
+      // {
+      //   name: 'exception',
+      //   icon: 'warning',
+      //   path: '/exception',
+      //   routes: [
+      //     // exception
+      //     {
+      //       path: '/exception/403',
+      //       name: 'not-permission',
+      //       component: './Exception/403',
+      //     },
+      //     {
+      //       path: '/exception/404',
+      //       name: 'not-find',
+      //       component: './Exception/404',
+      //     },
+      //     {
+      //       path: '/exception/500',
+      //       name: 'server-error',
+      //       component: './Exception/500',
+      //     },
+      //     {
+      //       path: '/exception/trigger',
+      //       name: 'trigger',
+      //       hideInMenu: true,
+      //       component: './Exception/TriggerException',
+      //     },
+      //   ],
+      // },
+      // {
+      //   name: 'account',
+      //   icon: 'user',
+      //   path: '/account',
+      //   routes: [
+      //     {
+      //       path: '/account/center',
+      //       name: 'center',
+      //       component: './Account/Center/Center',
+      //       routes: [
+      //         {
+      //           path: '/account/center',
+      //           redirect: '/account/center/articles',
+      //         },
+      //         {
+      //           path: '/account/center/articles',
+      //           component: './Account/Center/Articles',
+      //         },
+      //         {
+      //           path: '/account/center/applications',
+      //           component: './Account/Center/Applications',
+      //         },
+      //         {
+      //           path: '/account/center/projects',
+      //           component: './Account/Center/Projects',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       path: '/account/settings',
+      //       name: 'settings',
+      //       component: './Account/Settings/Info',
+      //       routes: [
+      //         {
+      //           path: '/account/settings',
+      //           redirect: '/account/settings/base',
+      //         },
+      //         {
+      //           path: '/account/settings/base',
+      //           component: './Account/Settings/BaseView',
+      //         },
+      //         {
+      //           path: '/account/settings/security',
+      //           component: './Account/Settings/SecurityView',
+      //         },
+      //         {
+      //           path: '/account/settings/binding',
+      //           component: './Account/Settings/BindingView',
+      //         },
+      //         {
+      //           path: '/account/settings/notification',
+      //           component: './Account/Settings/NotificationView',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
       {
         component: '404',
       },
