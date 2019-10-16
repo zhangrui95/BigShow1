20846b1de08facf53647612e60a72588b4a7f679
thkyo <thkyo@163.com>
2019-10-16 17:34:04 +0800

    机构管理增加增删改；设备管理增加系统状态、刷新

--- src/pages/MiddlePlatform/GroupTable.js
+++ src/pages/MiddlePlatform/GroupTable.js
@@ -1,8 +1,14 @@
 import React, { PureComponent } from 'react';
-import { Table } from 'antd';
+import { Table, Popconfirm, Divider } from 'antd';
 import styles from './GroupTable.less';
 
 class GroupTable extends PureComponent {
+    confirm = (params) => { // 确认删除
+        this.props.delGroup(params);
+    }
+    handleUpdDict = (record) => {
+        if (record) this.props.updGroup(record);
+    }
   render() {
     const { data } = this.props;
     const columns = [
@@ -14,6 +20,18 @@ class GroupTable extends PureComponent {
         title: '机构编码',
         dataIndex: 'code',
       },
+        {
+            title: '操作',
+            render: (text, record) => (
+                <div>
+                    <Popconfirm title={`确定要删除${record.name}`} onConfirm={() => this.confirm(record.id)} okText="确定" cancelText="取消">
+                        <a href="#">删除</a>
+                    </Popconfirm>
+                    <Divider type="vertical" />
+                    <a onClick={() => this.handleUpdDict(record)}>编辑</a>
+                </div>
+            ),
+        },
     ];
     return (
       <div className={styles.standardTable}>
